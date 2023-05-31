ee287260f3f53aaf50c9c9a65134b3ea9e82e029
Maxyboy50 <weitzmax15@gmail.com>
2023-05-30 21:40:03 -0500

    Updated source_hash value for lambda code object

--- terraform_code/s3.tf
+++ terraform_code/s3.tf
@@ -14,5 +14,6 @@ resource "aws_s3_object" "deployment_package" {
   bucket = data.aws_s3_bucket.this.id
   key    = "deployment_package.zip"
   source = var.lambda_code_zip_path
+  source_hash = filemd5(var.lambda_code_zip_path)
 
 }
