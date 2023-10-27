18c03e7737a97446be767dde5622973ec70759f7
Max Weitz <weitzmax15@gmail.com>
2023-10-26 23:51:41 -0500

    Building out tests for lambda

---
+++ tests/main_test.py
@@ -0,0 +1,20 @@
+import sys
+import unittest
+from lambda_files.chat_gpt_interactions import num_tokens_from_string 
+
+class TestTikToken(unittest.TestCase):
+    def test_tiktoken(self):
+        str = "Hello there from test case land!"
+        self.assertEqual(num_tokens_from_string(str, "gpt2"), 7)
+
+# class TestCommandHandler(unittest.TestCase):
+#     def test_longmessage_command(self): 
+#         command = "longmessage"
+
+
+#     def test_chatgptmessage_command(self): 
+#         command = "chatgptmessage"
+        
+
+if __name__ == '__main__':
+    unittest.main()
